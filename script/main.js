61492e3b3a63b3003d0642c84680f74a9300e206
杜传虎 <duchuanhu@duchuanhudeMacBook-Pro.local>
2018-06-19 21:22:14 +0800

    页面跳转

---
+++ script/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+});
+
+$("#nav_1").on("click",function () {
+    $(".main-page").hide();
+    $(".dot").hide();
+    $("#page_1").show();
+
+
+});
+
+$("#nav_2").on("click",function () {
+    $(".main-page").hide();
+    $(".dot").hide();
+    $("#page_2").show();
+    $("#dot_1").show();
+});
+
+$("#nav_3").on("click",function () {
+    $(".main-page").hide();
+    $(".dot").hide();
+    $("#page_3").show();
+    $("#dot_2").show();
+});
+
+$("#nav_4").on("click",function () {
+    $(".main-page").hide();
+    $(".dot").hide();
+    $("#page_4").show();
+    $("#dot_3").show();
+});
+
+$("#nav_5").on("click",function () {
+    $(".main-page").hide();
+    $(".dot").hide();
+    $("#page_5").show();
+    $("#dot_4").show();
+
+});
+
