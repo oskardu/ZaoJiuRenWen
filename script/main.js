9176a1049302378049213abac8051a5d326b630f
杜传虎 <duchuanhu@duchuanhudeMacBook-Pro.local>
2018-06-20 20:46:31 +0800

    滚动到顶部

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 });
 
-$("#nav_1").on("click",function () {
+$("#nav_1").on("click", function () {
     $(".main-page").hide();
     $(".dot").hide();
     $("#page_1").show();
@@ -9,28 +9,28 @@ $("#nav_1").on("click",function () {
 
 });
 
-$("#nav_2").on("click",function () {
+$("#nav_2").on("click", function () {
     $(".main-page").hide();
     $(".dot").hide();
     $("#page_2").show();
     $("#dot_1").show();
 });
 
-$("#nav_3").on("click",function () {
+$("#nav_3").on("click", function () {
     $(".main-page").hide();
     $(".dot").hide();
     $("#page_3").show();
     $("#dot_2").show();
 });
 
-$("#nav_4").on("click",function () {
+$("#nav_4").on("click", function () {
     $(".main-page").hide();
     $(".dot").hide();
     $("#page_4").show();
     $("#dot_3").show();
 });
 
-$("#nav_5").on("click",function () {
+$("#nav_5").on("click", function () {
     $(".main-page").hide();
     $(".dot").hide();
     $("#page_5").show();
@@ -38,3 +38,25 @@ $("#nav_5").on("click",function () {
 
 });
 
+$("#biansheng_home").on("click", function () {
+    $(".main-page").hide();
+    $(".dot").hide();
+    $("#page_2").show();
+    $("#dot_1").show();
+    toTop();
+
+});
+
+$("#wutai_home").on("click", function () {
+    $(".main-page").hide();
+    $(".dot").hide();
+    $("#page_3").show();
+    $("#dot_2").show();
+    toTop();
+
+});
+
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
